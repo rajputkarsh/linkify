af721fa5337b9aedd5fa80194bc852419b5c8b4b
rajput-karsh <utkarsh1@appinventiv.com>
2023-12-20 15:35:32 +0530

    add redirects

--- src/app/(website)/login/page.js
+++ src/app/(website)/login/page.js
@@ -1,12 +1,19 @@
-import LoginWithGoogle from "@/components/buttons/LoginWithGoogle";
+import LoginWithGoogle from '@/components/buttons/LoginWithGoogle';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const session = await getServerSession(authOptions);
+
+  if (session?.user?.name) {
+    redirect('/account');
+  }
+
   return (
     <div>
       <div className="p-4 max-w-xs mx-auto">
-        <h1 className="text-4xl font-bold text-center mb-2">
-          Sign In
-        </h1>
+        <h1 className="text-4xl font-bold text-center mb-2">Sign In</h1>
         <p className="text-center mb-6 text-gray-500">
           Sign in to your account using one of the methods below
         </p>
--- src/app/(website)/page.js
+++ src/app/(website)/page.js
@@ -1,9 +1,15 @@
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import HeroForm from '@/components/forms/HeroForm';
 import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
+
+  if (session?.user?.name) {
+    redirect('/account');
+  }
+
   return (
     <main>
       <section className="pt-32">
